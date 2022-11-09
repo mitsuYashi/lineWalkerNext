4c7829fc587cb43d1f499da538049ff481b174fd
mitsuYashi <hm20011027@gmail.com>
2022-11-09 15:00:03 +0900

    fix: extends

--- next-app/.eslintrc.js
+++ next-app/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ["plugin:import/recommended", "plugin:import/warnings"],
+  extends: ["plugin:import/recommended", "prettier"],
   overrides: [],
   parser: "@typescript-eslint/parser",
   parserOptions: {
