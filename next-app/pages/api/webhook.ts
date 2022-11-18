9a68543b8b0bec32e0149c86d67200969e367c13
mitsuYashi <hm20011027@gmail.com>
2022-11-18 11:08:21 +0900

    change: webhook message

--- next-app/pages/api/webhook.ts
+++ next-app/pages/api/webhook.ts
@@ -45,7 +45,7 @@ const handler: NextApiHandler = async (
                   );
                   await line.client.replyMessage(event.replyToken, {
                     type: "text",
-                    text: `対応外のメッセージには返答していません`,
+                    text: `対応外のメッセージです${event.message}`,
                   });
                   break;
 
