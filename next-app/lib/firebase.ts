4d4a8659a2c3292ba7cc4ac6c06d248de725ae2b
mitsuYashi <hm20011027@gmail.com>
2022-11-28 13:06:01 +0900

    feat: login

--- next-app/lib/firebase.ts
+++ next-app/lib/firebase.ts
@@ -1,7 +1,12 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+  getAuth,
+  signOut,
+  GoogleAuthProvider,
+  signInWithPopup,
+  User,
+} from "firebase/auth";
+import { FirebaseResponse } from "type";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,4 +19,50 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+
+export const signInAuth = () =>
+  new Promise<FirebaseResponse>((resolve, reject) => {
+    const provider = new GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/fitness.location.read");
+    provider.setCustomParameters({
+      clientId: process.env.NEXT_PUBLIC_client_id!,
+      clientSecret: process.env.NEXT_PUBLIC_client_secret!,
+      redirectUri: process.env.NEXT_PUBLIC_redirect_uris!,
+    });
+    console.log(process.env.NEXT_PUBLIC_client_id);
+    console.log(process.env.NEXT_PUBLIC_client_secret);
+    console.log(process.env.NEXT_PUBLIC_redirect_uris);
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential?.accessToken;
+        const user = result.user;
+        console.log(user);
+        resolve(user as unknown as FirebaseResponse);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(error);
+      });
+  });
+
+export const signOutAuth = () => {
+  const auth = getAuth();
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+      console.log(error);
+    });
+};
+
+export const userToken = async () => {
+  return await getAuth(app).currentUser?.getIdToken();
+};
