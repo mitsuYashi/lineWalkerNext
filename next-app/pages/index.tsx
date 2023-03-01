4a68a85896a917cf8774b9ed74a4c8a2d836eebd
mitsuYashi <hm20011027@gmail.com>
2023-03-01 15:57:23 +0900

    delete: library

--- next-app/pages/index.tsx
+++ next-app/pages/index.tsx
@@ -1,10 +1,17 @@
+import axios from "axios";
+import { Router } from "express";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  const handleClick = () => {};
+  const router = useRouter();
+  const handleClick = async () => {};
 
   return (
     <div>
       <h1>Line Walker</h1>
-      <button onClick={handleClick}>login</button>
+      <button onClick={handleClick}>
+        <a href="https://linewalker.onrender.com/user">login</a>
+      </button>
     </div>
   );
 }
